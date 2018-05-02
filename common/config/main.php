f4a7d051df0a9fa0dd2f949f9662f50019dbb8c1
zigfried123 <zigfried123@mail.ru>
2018-05-02 15:42:53 +0400

    added

--- common/config/main.php
+++ common/config/main.php
@@ -1,5 +1,6 @@
 <?php
 return [
+    'language' => 'ru-RU',
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
@@ -9,5 +10,16 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        'i18n' => [
+            'translations'=>[
+                // app* - это шаблон, который определяет, какие категории
+                // обрабатываются источником. В нашем случае, мы обрабатываем все, что начинается с app
+                '*'=>[
+                    'class'=>yii\i18n\DbMessageSource::className(),
+                    'sourceLanguage'=>'en-EN',
+                ],
+            ]
+        ],
     ],
 ];
+
---
+++ common/messages/ru-RU/ref.php
@@ -0,0 +1,4 @@
+<?php
+return [
+  'You came from' => 'Вы пришли от',
+];
